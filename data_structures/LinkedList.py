8114885d75cbc86768a51fc056e5a610d6428418
pranayom <pranaystb@gmail.com>
2024-08-11 09:05:41 -0400

    Adding challenge 1 code

---
+++ data_structures/LinkedList.py
@@ -0,0 +1,32 @@
+from LinkedListNode import LinkedListNode
+
+
+#Template for Linked List
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_node_at_head(self, node):
+        if self.head:
+            node.next = self.head
+            self.head = node
+        else:
+            self.head = node
+    
+    def create_linked_list(self, lst):
+        for x in reversed(lst):
+            new_node = LinkedListNode(x)
+            self.insert_node_at_head(new_node)
+    
+    def __str__(self):
+        result = ""
+        temp = self.head
+        while temp:
+            result += str(temp.data)
+            temp = temp.next
+            if temp:
+                result += ", "
+        result += ""
+        return result
+        

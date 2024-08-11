673324ce4c07a070560e23ca24c38397b309db4d
pranayom <pranaystb@gmail.com>
2024-08-11 09:14:12 -0400

    Print linked list code added

---
+++ data_structures/PrintList.py
@@ -0,0 +1,12 @@
+
+def print_list_with_forward_arrow(linked_list_node):
+    temp = linked_list_node
+    while temp:
+        print(temp.data, end=" ")  # print node value
+        
+        temp = temp.next
+        if temp:
+            print("→", end=" ")
+        else:
+            # If this is the last node, print null at the end
+            print("→ null", end=" ")
